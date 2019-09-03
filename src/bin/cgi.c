e30c62b486d56b2e1d2fb6c1e27492dbdb09b71c
Tim Morgan <tim@timmorgan.org>
2019-09-03 23:14:47 +0000

    Improve cgi output

--- src/bin/cgi.c
+++ src/bin/cgi.c
@@ -4,6 +4,37 @@
 
 #include "../boardslam.h"
 
+void print_form(int n1, int n2, int n3) {
+  if (n1 != 0 && n2 != 0 && n3 != 0) {
+    printf(
+      "<form>"
+        "n1: "
+        "<input name='n1' size='2' value='%d'> "
+        "n2: "
+        "<input name='n2' size='2' value='%d'> "
+        "n3: "
+        "<input name='n3' size='2' value='%d'> "
+        "<input type='submit' value='Go!'>"
+      "</form>",
+      n1,
+      n2,
+      n3
+    );
+  } else {
+    printf(
+      "<form>"
+        "n1: "
+        "<input name='n1' size='2' value=''> "
+        "n2: "
+        "<input name='n2' size='2' value=''> "
+        "n3: "
+        "<input name='n3' size='2' value=''> "
+        "<input type='submit' value='Go!'>"
+      "</form>"
+    );
+  }
+}
+
 int main() {
   printf("Content-Type: text/html\n\n");
   printf(
@@ -17,25 +48,20 @@ int main() {
         "<h1>Board Slam / N2K Math Game Calculator</h1>"
         "<p><em>written April 2018 (updated September 2019)</em></p>"
         "<p>"
-          "Classical Conversations (Homeschool) communities all over play a math game in the classroom called \"Board Slam\""
-          "or \"National Number Knockout\" (N2K). Participants try to use 3 numbers determined by dice roll along with"
-          "various mathematical operations to calculate the answers to fill a grid (the board) of one (1) through"
+          "Classical Conversations (Homeschool) communities all over play a math game in the classroom called \"Board Slam\" "
+          "or \"National Number Knockout\" (N2K). Participants try to use 3 numbers determined by dice roll along with "
+          "various mathematical operations to calculate the answers to fill a grid (the board) of one (1) through "
           "thirty-six (36) slots. This is great fun!"
         "</p>"
         "<p>"
-          "But sometimes, students and parents alike get stuck, wondering if they've truly exhaused all possibilities."
-          "This page will show you possible ways to calculate the answers using the numbers given, and at the bottom of the"
+          "But sometimes, students and parents alike get stuck, wondering if they've truly exhaused all possibilities. "
+          "This page will show you possible ways to calculate the answers using the numbers given, and at the bottom of the "
           "page, it will tell you which answers seem to be impossible (if any)."
         "</p>"
-        "<form>"
-          "n1:"
-          "<input name='n1' size='2' value=''>"
-          "n2:"
-          "<input name='n2' size='2' value=''>"
-          "n3:"
-          "<input name='n3' size='2' value=''>"
-          "<input type='submit' value='Go!'>"
-        "</form>"
+        "<p>"
+          "This tool only tries powers up to 3 and does not attempt fractional exponents. "
+          "The sourcecode <a href='https://github.com/seven1m/boardslam-c'>is available</a> in case you want to help improve it!"
+        "</p>"
   );
   char *q = getenv("QUERY_STRING");
   char *n1_buf, *n2_buf, *n3_buf;
@@ -58,6 +84,7 @@ int main() {
       int n2 = atoi(n2_buf);
       int n3 = atoi(n3_buf);
       if (n1 >= 1 && n1 <= 6 && n2 >= 1 && n2 <= 6 && n3 >= 1 && n3 <= 6) {
+        print_form(n1, n2, n3);
         Answer *answers = boardslam_results(n1, n2, n3);
         printf("<pre>");
         for (size_t i=0; i<36; i++) {
@@ -72,18 +99,24 @@ int main() {
           }
         }
         printf("\nmissing: ");
-        int comma = 0;
+        int at_least_one = 0;
         for (size_t i=0; i<36; i++) {
           if (answers[i].x.number == 0) {
-            if (comma) printf(", ");
-            comma = 1;
+            if (at_least_one) printf(", ");
+            at_least_one = 1;
             printf("%lu", i+1);
           }
         }
+        if (!at_least_one) {
+          printf("none");
+        }
         printf("</pre>");
       } else {
-        printf("You must supply 3 numbers, each between 1 and 6.\n");
+        print_form(0, 0, 0);
+        printf("<p>You must supply 3 numbers, each between 1 and 6.</p>");
       }
+    } else {
+      print_form(0, 0, 0);
     }
   }
   printf("</body></html>");
